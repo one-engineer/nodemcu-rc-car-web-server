04a7a35f656adeb37d2b3b88585ebd6f6c8934e1
Süm <57527976+one-engineer@users.noreply.github.com>
2021-02-17 21:20:34 +0300

    Update code.ino

--- code/code.ino
+++ code/code.ino
@@ -3,7 +3,7 @@
 
 ESP8266WebServer server(80); // 80 numaralı port
 
-// wifi kurulumu
+
 const int ena = 14;
 const int enb = 12;
 const int in1 = 5;
@@ -11,11 +11,11 @@ const int in2 = 4;
 const int in3 = 0;
 const int in4 = 2;
 
-int speedCar = 1000;  
-
-char ssid[] = "****";
-char pass[] = "****";
-char HTML[] = "<!DOCTYPE html> <head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\"><style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}.button { background-color: #6df996; border: none; color: white; padding: 25px 25px;text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}.button2 {background-color: #9effba;}</style> </head> <body> <h1>WifiArac</h1> <button class=\"button\"><a href=\"ileri\">Go</a></button> <br> <button class=\"button\" ><a href=\"sol\">Left</a></button> <button class=\"button button2\" ><a href=\"dur\">Stop</a></button> <button class=\"button\"><a href=\"sag\">Right</a></button> <br> <button class=\"button\"><a href=\"geri\">Back</a></button> <h2> Kontrol et </h2> </body></html>"; // --> Simple HTML page
+int speedCar=450;  
+// wifi kurulumu
+char ssid[] ="";
+char pass[] = "";
+char HTML[] = " <!DOCTYPE html>  <head> <meta name=\'viewport\' content=\'width=device-width, initial-scale=1\'></head>  <style> html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}.button { background-color: #6df996; border: none;  color: white;  padding: 25px 25px; text-decoration: none;  font-size: 30px;  margin: 2px;  cursor: pointer; } .button2 {background-color: #9effba; } </style> <body>  <h1>RC Car - IOT</h1>  <button class=\'button\'><a href=\'ileri\'>ILERI</a></button> <br> <button class=\'button\' ><a href=\'sol\'>SOL</a></button>  <button class=\'button button2\' ><a href=\'dur\'>DUR</a></button>  <button class=\'button\'><a href=\'sag\'>SAG</a></button>  <br> <button class=\'button\'><a href=\'geri\'>GERI</a></button>  <h2> Hiz Miktari </h2>  <button class=\'button\'><a href=\'yavas\'>Yavas</a></button> <button class=\'button\'><a href=\'orta\'>Orta</a></button> <button class=\'button\'><a href=\'hizli\'>Hizli</a></button> </body></html>";
 
 void handleRoot() {
   server.send(200,"text / html", HTML);
@@ -55,6 +55,10 @@ server.on("/sol",sol);
 server.on("/sag",sag);
 server.on("/geri",geri);
 server.on("/dur",dur);
+
+server.on("/yavas",yavas);
+server.on("/orta",orta);
+server.on("/hizli",hizli);
 server.begin();
 }
 
@@ -63,9 +67,21 @@ void loop(){
  
   
 }
-
+void yavas(){
+  speedCar = 300;
+  server.send(200,"text/html",HTML);
+}
+void orta(){
+ speedCar = 600;
+ server.send(200,"text/html",HTML);
+}
+void hizli(){
+   speedCar = 1000;
+   server.send(200,"text/html",HTML);
+  
+}
 void ileri(){
-  Serial.println("Motor ileri");
+  Serial.println("\nMotor ileri");
   server.send(200,"text/html",HTML);
 
    digitalWrite(in1, LOW);
@@ -75,10 +91,11 @@ void ileri(){
    digitalWrite(in3, HIGH);
    digitalWrite(in4, LOW);
    analogWrite(enb, speedCar);
- 
+  Serial.print("Motor hizi: ");
+  Serial.print(speedCar);
 }
 void geri(){
-  Serial.println("Motor geri");
+  Serial.println("\nMotor geri");
   server.send(200,"text/html",HTML);
 
   digitalWrite(in1, HIGH);
@@ -88,9 +105,11 @@ void geri(){
   digitalWrite(in3, LOW);
   digitalWrite(in4, HIGH);
   analogWrite(enb, speedCar);
+  Serial.print("Motor hizi: ");
+  Serial.print(speedCar);
 }
 void sag(){
-  Serial.println("Motor sag");
+  Serial.println("\nMotor sag");
   server.send(200,"text/html",HTML);
 
    digitalWrite(in1, HIGH);
@@ -100,9 +119,11 @@ void sag(){
    digitalWrite(in3, HIGH);
    digitalWrite(in4, LOW);
    analogWrite(enb, speedCar);
+  Serial.print("Motor hizi: ");
+  Serial.print(speedCar);
 }
 void sol(){
-  Serial.println("Motor sol");
+  Serial.println("\nMotor sol");
   server.send(200,"text/html",HTML);
 
 
@@ -113,11 +134,14 @@ void sol(){
   digitalWrite(in3, LOW);
   digitalWrite(in4, HIGH);
   analogWrite(enb, speedCar);
+  Serial.print("Motor hizi: ");
+  Serial.print(speedCar);
+  
   }
 
 
 void dur(){
-  Serial.println("Motor dur");
+  Serial.println("\nMotor dur");
   server.send(200,"text/html",HTML);
 
   digitalWrite(in1, LOW);
@@ -127,4 +151,6 @@ void dur(){
   digitalWrite(in3, LOW);
   digitalWrite(in4, LOW);
   analogWrite(enb, speedCar);
+  Serial.print("\nMotor hizi: ");
+  Serial.print(speedCar);
 }
